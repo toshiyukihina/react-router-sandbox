9e73f03787e57089ffe9885aba693837e625d338
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2016-04-22 11:59:18 +0900

    lesson 5: 'Active Links' is done

--- app/assets/javascripts/components/app.js.jsx
+++ app/assets/javascripts/components/app.js.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router';
+import NavLink from './nav_link';
 
 class App extends React.Component {
 
@@ -8,8 +8,8 @@ class App extends React.Component {
       <div>
         <h1>Ghettohub Issues</h1>
         <ul role="nav">
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/repos">Repos</Link></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/repos">Repos</NavLink></li>
         </ul>
         {this.props.children}
       </div>
