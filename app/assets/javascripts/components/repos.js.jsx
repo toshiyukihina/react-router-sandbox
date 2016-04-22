e2c375f51137625e47eea4c656300d89814c448f
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2016-04-22 13:08:30 +0900

    lesson 12: 'Navigating Programatically' is done

--- app/assets/javascripts/components/repos.js.jsx
+++ app/assets/javascripts/components/repos.js.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 import NavLink from './nav_link';
+import {browserHistory} from 'react-router';
 
 class Repos extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { userName: '', repo: '' };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleUserNameChange = this.handleUserNameChange.bind(this);
+    this.handleRepoChange = this.handleRepoChange.bind(this);
+  }
+
+  handleUserNameChange(e) {
+    this.setState({userName: e.target.value});
+  }
+
+  handleRepoChange(e) {
+    this.setState({repo: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const userName = this.state.userName;
+    const repo = this.state.repo;
+    const path = `/repos/${userName}/${repo}`;
+    this.context.router.push(path);
+  }
 
   render() {
     return (
@@ -10,6 +37,13 @@ class Repos extends React.Component {
         <ul>
           <li><NavLink to="/repos/reactjs/react-router">ReactRouter</NavLink></li>
           <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="userName" onChange={this.handleUserNameChange} /> / {' '}
+              <input type="text" placeholder="repo" onChange={this.handleRepoChange} />{' '}
+              <button type="submit">Go</button>
+            </form>
+          </li>
         </ul>
         {this.props.children}
       </div>
@@ -17,5 +51,9 @@ class Repos extends React.Component {
   }
   
 }
+
+Repos.contextTypes = {
+  router: React.PropTypes.object
+};
 
 export default Repos;
